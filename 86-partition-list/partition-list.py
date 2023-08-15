8876981f34fc7353ae54487bd2932e1214c3be1d
joyoustreasure <138423654+joyoustreasure@users.noreply.github.com>
2023-08-15 12:03:01 +0900

    Time: 37 ms (93.69%) | Memory: 16.3 MB (84.23%) - LeetSync

--- 86-partition-list/partition-list.py
+++ 86-partition-list/partition-list.py
@@ -7,15 +7,15 @@ class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
         part1=ListNode(-1000)
         part2=ListNode(-1000)
-        cur = head
+        
         cur_1,cur_2=part1,part2
-        while cur:
-            if cur.val<x:
-                cur_1.next=ListNode(cur.val)
+        while head:
+            if head.val<x:
+                cur_1.next=ListNode(head.val)
                 cur_1=cur_1.next
             else:
-                cur_2.next=ListNode(cur.val)
+                cur_2.next=ListNode(head.val)
                 cur_2=cur_2.next
-            cur=cur.next
+            head=head.next
         cur_1.next=part2.next
         return part1.next
