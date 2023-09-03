83af9b7d6502536b37043f2a92ff26945e7cda25
joyoustreasure <138423654+joyoustreasure@users.noreply.github.com>
2023-09-03 22:46:05 +0900

    Time: 42 ms (24.62%) | Memory: 13.8 MB (47.71%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -1,28 +1,49 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
-    // 주어진 값들의 누적 합에 대해 각 질의를 어디에서 찾을 수 있는지 알려주는 함수입니다.
-    vector<int> answerQueries(vector<int>& v, vector<int>& q) {
-        // 먼저 v 벡터를 정렬합니다.
-        sort(v.begin(), v.end());
-        
-        // v의 각 요소에 대해 누적 합을 계산합니다.
-        for(int i=1; i<v.size(); i++) {
-            v[i] += v[i-1];
+    // 간단한 삽입 정렬을 사용하여 벡터를 정렬합니다.
+    void mySort(vector<int>& v) {
+        for (int i = 1; i < v.size(); i++) {
+            int key = v[i];
+            int j = i - 1;
+            while (j >= 0 && v[j] > key) {
+                v[j + 1] = v[j];
+                j--;
+            }
+            v[j + 1] = key;
         }
-        
+    }
+
+    // upper_bound 함수를 직접 구현합니다.
+    int myUpperBound(const vector<int>& v, int x) {
+        int l = 0, r = v.size();
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (v[mid] <= x)
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return l;
+    }
+
+    vector<int> answerQueries(vector<int>& v, vector<int>& q) {
+        mySort(v); // 정렬 함수를 직접 호출
+
+        for (int i = 1; i < v.size(); i++) {
+            v[i] += v[i - 1];
+        }
+
         vector<int> ans;
-        // 각 질의에 대해 이진 검색을 사용하여 해당 값을 초과하는 첫 번째 위치를 찾습니다.
-        for(int x: q) {
-            int idx = upper_bound(v.begin(), v.end(), x) - v.begin();
+        for (int x : q) {
+            int idx = myUpperBound(v, x); // upper_bound 함수를 직접 호출
             ans.push_back(idx);
         }
-        
+
         return ans;
     }
 };
@@ -34,7 +55,7 @@ public:
 
 //     vector<int> result = sol.answerQueries(v, q);
 
-//     for(int i : result) {
+//     for (int i : result) {
 //         cout << i << " ";
 //     }
 //     // 예상 출력: 2 4 5
