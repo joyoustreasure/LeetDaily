3e052df011d491b8f818bbb1bf2ce7e194182488
joyoustreasure <138423654+joyoustreasure@users.noreply.github.com>
2023-09-03 22:35:05 +0900

    Time: 12 ms (78.02%) | Memory: 11.1 MB (34.74%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.cpp
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.cpp
@@ -1,34 +1,37 @@
-
-
-
-//ver2 algorithm 사용
 #include <iostream>
 #include <vector>
-#include <algorithm> // max 함수를 위해 필요합니다.
 
 using namespace std;
 
 class Solution 
 {
 public:
+    // 주어진 숫자들 중에서 최대값을 반환하는 함수
+    int maxInGrid(int i, int j, vector<vector<int>>& grid) {
+        int max_val = grid[i-1][j-1];
+        max_val = max(max_val, grid[i-1][j]);
+        max_val = max(max_val, grid[i-1][j+1]);
+        max_val = max(max_val, grid[i][j-1]);
+        max_val = max(max_val, grid[i][j]);
+        max_val = max(max_val, grid[i][j+1]);
+        max_val = max(max_val, grid[i+1][j-1]);
+        max_val = max(max_val, grid[i+1][j]);
+        max_val = max(max_val, grid[i+1][j+1]);
+        return max_val;
+    }
+
     // 3x3 부분 그리드의 최대 값을 찾는 함수입니다.
     vector<vector<int>> largestLocal(vector<vector<int>>& grid) 
     {
         int n = grid.size();
         vector<vector<int>> res(n-2, vector<int> (n-2));
         
-        // 3x3 그리드의 중심을 기준으로 최대 값을 찾습니다.
-        // 예: (1,1)에서 시작하면 (1,1),(1,2),(2,1),(2,2)는 각각 3x3 그리드의 중심이 됩니다.
+        // 3x3 그리드의 모든 값을 고려하여 최대 값을 찾습니다.
         for(int i = 1; i <= n-2; i++)
         {
             for(int j = 1; j <= n-2; j++)
             {
-                int maxi = 0;
-                maxi = max({maxi, grid[i-1][j-1], grid[i-1][j], grid[i-1][j+1]});
-                maxi = max({maxi, grid[i][j-1], grid[i][j], grid[i][j+1]});
-                maxi = max({maxi, grid[i+1][j-1], grid[i+1][j], grid[i+1][j+1]});
-                
-                res[i-1][j-1] = maxi;
+                res[i-1][j-1] = maxInGrid(i, j, grid);
             }
         }
         return res;
@@ -38,10 +41,11 @@ public:
 // int main() {
 //     Solution sol;
 //     vector<vector<int>> grid = {
-//         {1, 2, 3, 4},
-//         {5, 6, 7, 8},
-//         {9, 10, 11, 12},
-//         {13, 14, 15, 16}
+//         {1, 1, 1, 1, 1},
+//         {1, 1, 1, 1, 1},
+//         {1, 1, 2, 1, 1},
+//         {1, 1, 1, 1, 1},
+//         {1, 1, 1, 1, 1}
 //     };
 
 //     vector<vector<int>> result = sol.largestLocal(grid);
