82a6a3fc9d6fc12525181215155c9a123f67b48a
joyoustreasure <138423654+joyoustreasure@users.noreply.github.com>
2023-09-18 23:34:54 +0900

    Time: 12 ms (100.00%) | Memory: 11.5 MB (13.41%) - LeetSync

--- 2502-sort-the-people/sort-the-people.c
+++ 2502-sort-the-people/sort-the-people.c
@@ -2,46 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Person {
-    char name[100];
+typedef struct {
+    char* name;
     int height;
-};
+} Person;
 
-void bubbleSort(struct Person* people, int size) {
-    int i, j;
-    struct Person temp;
-
-    for (i = 0; i < size - 1; i++) {
-        for (j = 0; j < size - i - 1; j++) {
-            if (people[j].height < people[j + 1].height) {
-                // 키를 기준으로 내림차순으로 정렬
-                temp = people[j];
-                people[j] = people[j + 1];
-                people[j + 1] = temp;
-            }
-        }
-    }
+int compare(const void *a, const void *b) {
+    return ((Person*)b)->height - ((Person*)a)->height; // descending order
 }
 
-char** sortPeople(char** names, int namesSize, int* heights, int heightsSize, int* returnSize) {
-    // 사람들을 구조체 배열로 변환하여 정렬
-    struct Person* people = malloc(namesSize * sizeof(struct Person));
-    for (int i = 0; i < namesSize; i++) {
-        strcpy(people[i].name, names[i]);
+char** sortPeople(char **names, int namesSize, int* heights, int heightsSize, int* returnSize) {
+    // Check if namesSize and heightsSize are equal. If not, return NULL.
+    if (namesSize != heightsSize) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    // Create an array of Person structures
+    Person people[namesSize];
+    for(int i = 0; i < namesSize; i++) {
+        people[i].name = names[i];
         people[i].height = heights[i];
     }
 
-    // 버블 정렬로 구조체 배열 정렬
-    bubbleSort(people, namesSize);
+    // Sort the array of structures based on height
+    qsort(people, namesSize, sizeof(Person), compare);
 
-    // 정렬된 결과를 다시 문자열 배열로 변환하여 반환
-    char** sortedNames = malloc(namesSize * sizeof(char*));
-    for (int i = 0; i < namesSize; i++) {
-        sortedNames[i] = malloc((strlen(people[i].name) + 1) * sizeof(char));
-        strcpy(sortedNames[i], people[i].name);
+    // Create an array to store sorted names
+    char** sortedNames = (char**)malloc(namesSize * sizeof(char*));
+    for(int i = 0; i < namesSize; i++) {
+        sortedNames[i] = people[i].name;
     }
-
-    free(people);
+    
     *returnSize = namesSize;
     return sortedNames;
 }
